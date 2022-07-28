aa9653d2619b5f4a1bee16530e641ded3b42a066
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-07-28 15:24:16 -0400

    importing csv of training and eval data

--- index.py
+++ index.py
@@ -8,3 +8,9 @@ import urllib
 import tensorflow as tf
 import tensorflow.compat.v2.feature_column as fc
 
+# Load dataset
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
+print(dftrain.loc[0], y_train.loc[0])

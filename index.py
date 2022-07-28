41088af7c80772aedf45d9c7ad0b56b9566108d3
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-07-28 15:02:42 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import urllib
+
+import tensorflow as tf
+import tensorflow.compat.v2.feature_column as fc
+

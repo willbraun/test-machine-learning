a5387f113c3e16c39aad8f4b659ed38460ee4963
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-07-29 14:54:44 -0400

    completed linear regression tutorial

--- index.py
+++ index.py
@@ -20,8 +20,7 @@ y_eval = dfeval.pop('survived')
 # pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
 # plt.show()
 
-CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
-                       'embark_town', 'alone']
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck', 'embark_town', 'alone']
 NUMERIC_COLUMNS = ['age', 'fare']
 
 feature_columns = []
@@ -32,4 +31,22 @@ for feature_name in CATEGORICAL_COLUMNS:
 for feature_name in NUMERIC_COLUMNS:
     feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
-print(feature_columns)
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+        if shuffle:
+            ds = ds.shuffle(1000)
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds
+    return input_function
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+linear_est.train(train_input_fn)
+result = list(linear_est.predict(eval_input_fn))
+
+print(dfeval.loc[0])
+print(y_eval[0])
+print(result[0]['probabilities'][1])

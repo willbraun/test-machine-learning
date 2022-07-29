5e1c8c6e61278eee220d8caabb1fcce3c1bf5175
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-07-28 23:26:54 -0400

    adding feature columns

--- index.py
+++ index.py
@@ -13,4 +13,23 @@ dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.
 dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
-print(dftrain.loc[0], y_train.loc[0])
+
+# dftrain.age.hist(bins=20)
+# dftrain.sex.value_counts().plot(kind='barh')
+# dftrain['class'].value_counts().plot(kind='barh')
+# pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
+# plt.show()
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
+                       'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+    vocabulary = dftrain[feature_name].unique()
+    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+print(feature_columns)

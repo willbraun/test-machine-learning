03ba1da52027479f0a3cf4d0a837c21e7eb87334
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-07-31 23:22:59 -0400

    completed flower classification tutorial

--- classification.py
+++ classification.py
@@ -3,22 +3,78 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 import pandas as pd
 
-CSV_COLUMN_NAMES = ['SepalLength', 'Sepalwidth", "PetalLength", "Petalwidth", "Species']
+CSV_COLUMN_NAMES = ['SepalLength', 'Sepalwidth', 'PetalLength', 'Petalwidth', 'Species']
 SPECIES = ['Setosa', 'Versicolor', 'Virginica']
 # defining constants to help later on
 
 train_path = tf.keras.utils.get_file(
     "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
-test_path= tf.keras.utils.get_file(
+test_path = tf.keras.utils.get_file(
     "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
 train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
 test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
 #Here we use keras (a module inside of TensorFlow) to grab our datasets and read them into a pandas dataframe
 
-print(train.head())
-
 train_y = train.pop('Species')
-test_y = train.pop('Species')
+test_y = test.pop('Species')
 
-print(train.head())
-print(train.shape)
+# Input function
+def input_fn(features, labels, training=True, batch_size=256):
+    """An input function for training or evaluating"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle and repeat if you are in training mode.
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+
+    return dataset.batch(batch_size)
+
+# Feature columns describe how to use the input.
+my_feature_columns = []
+for key in train.keys():
+    my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+
+# Build a DNN with 2 hidden layers with 30 and 10 hidden nodes each.
+classifier = tf.estimator.DNNClassifier(
+    feature_columns=my_feature_columns,
+    # Two hidden layers of 30 and 10 nodes respectively.
+    hidden_units=[30, 10],
+    # The model must choose between 3 classes.
+    n_classes=3)
+
+# Train the Model.
+classifier.train(
+    input_fn=lambda: input_fn(train, train_y, training=True),
+    steps=5000)
+
+eval_result = classifier.evaluate(
+    input_fn=lambda: input_fn(test, test_y, training=False))
+
+# print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+def input_fn2(features, batch_size=256):
+    # Convert the inputs to a Dataset without labels
+    return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+features =['SepalLength', 'Sepalwidth', 'PetalLength', 'Petalwidth']
+predict = {}
+
+print('Please type the numeric values as prompted.')
+for feature in features:
+    valid = True
+    while valid:
+        val = input(feature + ": ")
+        if not val.isdigit(): valid = False
+
+    predict[feature] = [float(val)]
+
+predictions = classifier.predict(input_fn=lambda: input_fn2(predict))
+for pred_dict in predictions:
+    print(pred_dict)
+    class_id = pred_dict['class_ids'][0]
+    probability = pred_dict['probabilities'][class_id]
+
+    print('Prediction is "{}" ({:.1f}%)'.format(
+        SPECIES[class_id], 100 * probability
+    ))

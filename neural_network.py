a45bbf2b40907260c259573df579cdff5a9dc78a
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-08-02 22:58:47 -0400

    starting neural network tutorial

---
+++ neural_network.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fashion_mnist = keras.datasets.fashion_mnist # Load Fashion MNIST dataset
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() # split into training and testing
+print(train_images.shape)
+print(train_images[0, 23, 23])
+print(train_labels[:10])
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal", "Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# show test image with pyplot
+plt.figure()
+plt.imshow(train_images[1])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+

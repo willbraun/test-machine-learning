f9281168cd6a666e9c72c197508aa49910dc4011
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-08-03 21:30:47 -0400

    removed a print statement

--- neural_network.py
+++ neural_network.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 fashion_mnist = keras.datasets.fashion_mnist # Load Fashion MNIST dataset
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() # split into training and testing
-print(train_images[:5])
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 

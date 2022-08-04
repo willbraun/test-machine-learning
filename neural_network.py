26c3f03c02d93c4cc6af45689651015708ae0c2f
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-08-03 21:06:17 -0400

    created model and ran test data with it

--- neural_network.py
+++ neural_network.py
@@ -7,19 +7,32 @@ import matplotlib.pyplot as plt
 
 fashion_mnist = keras.datasets.fashion_mnist # Load Fashion MNIST dataset
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() # split into training and testing
-print(train_images.shape)
-print(train_images[0, 23, 23])
-print(train_labels[:10])
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal", "Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # show test image with pyplot
-plt.figure()
-plt.imshow(train_images[1])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+# plt.figure()
+# plt.imshow(train_images[1])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
 
 # Data preprocessing to get data between 0 and 1
 train_images = train_images / 255.0
 test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)), # input layer
+    keras.layers.Dense(128, activation='relu'), # hidden layer
+    keras.layers.Dense(10, activation='softmax') # output layer. Softmax makes sure layer neurons are less than 1 adds up to 1.
+])
+
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy', 
+              metrics=['accuracy']) # these are hyperparameters
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
+
+print('Test accuracy:', test_acc)
